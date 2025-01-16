b63bdd62821e3f754c2fc1b75cd46fffe7f2c63f
LoafyLH <poizzyroblox@gmail.com>
2025-01-16 15:38:21 +0800

    Reformatted BoardingGate + Terminal
    Implemented some functions for both classes
    
    Non implemented methods now throw NotImplementedException

--- Classes/BoardingGate.cs
+++ Classes/BoardingGate.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PRG2_T13_03;
+using PRG2_T13_03.Classes;
+using PRG2_T13_03.Classes.Flights;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,33 +11,35 @@ using System.Threading.Tasks;
 class BoardingGate
 {
     // Properties
-    public string GateName { get; set; }
+    public string GateName { get; set; } = "";
     public bool SupportsCFFT { get; set; }
     public bool SupportsDDJB { get; set; }
     public bool SupportsLWTT {  get; set; }
-    public Flight flight { get; set; }
-
-    // Constructor
-    public BoardingGate() { } // default constructor
-    public BoardingGate() // parameterized consturctor
-    {
-        //
-    }
+    public Flight Flight { get; set; }
 
     // Methods
     public double CalculateFees()
     {
-        //
+        throw new NotImplementedException();
     }
 
     public override string ToString()
     {
-        //
+        throw new NotImplementedException();
     }
 
-
-
-
-
-
+    // Constructor
+    public BoardingGate() 
+    {
+        Flight = new NORMFlight();
+    } 
+    // default constructor
+    public BoardingGate(string gateName, bool supportsCFFT, bool supportDDJB, bool supportsLWTT, Flight flight) // parameterized consturctor
+    {
+        GateName = gateName;
+        SupportsCFFT = supportsCFFT;
+        SupportsDDJB = supportDDJB;
+        SupportsLWTT = supportsLWTT;
+        Flight = flight;
+    }
 }
--- Classes/Terminal.cs
+++ Classes/Terminal.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PRG2_T13_03.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,44 +10,53 @@ using System.Threading.Tasks;
 class Terminal
 {
     // Properties
-    public string TerminalName { get; set; }
-    public Dictionary<string, Airline> Airlines { get; set; }
-    public Dictionary<string, Flight> Flights { get; set; }
-    public Dictionary<string, BoardingGate> BoardingGates { get; set; }
-    public Dictionary<string, double> GateFees { get; set; }
+    public string TerminalName { get; set; } = "";
+    public Dictionary<string, Airline> Airlines { get; set; } = new Dictionary<string, Airline>();
+    public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+    public Dictionary<string, BoardingGate> BoardingGates { get; set; } = new Dictionary<string, BoardingGate>();
+    public Dictionary<string, double> GateFees { get; set; } = new Dictionary<string, double>();
 
 
     // Constructor
     public Terminal() { }
-    public Terminal()
+    public Terminal(string terminalName)
     {
-        //
+        TerminalName = terminalName;
     }
 
     // Methods
     public bool AddAirline(Airline airline)
     {
-        //
+        return Airlines.TryAdd(airline.Code, airline);
     }
 
     public bool AddBoardingGate(BoardingGate boardingGate)
     {
-        //
+        return BoardingGates.TryAdd(boardingGate.GateName, boardingGate);
     }
 
     public Airline GetAirlineFromFlight(Flight flight)
     {
-        //
+        foreach (KeyValuePair<string, Airline> kvp in Airlines)
+        {
+            if (kvp.Value.Flights.ContainsKey(flight.FlightNumber))
+            {
+                return kvp.Value;
+            }
+        }
+
+        // Throws an error is there is somehow a flight without a airline
+        throw new Exception("Passed flight does not have a airline!");
     }
 
     public void PrintAirlineFees()
     {
-        // 
+        throw new NotImplementedException();
     }
 
     public override string ToString()
     {
-        //
+        return $"Terminal Name: {TerminalName}";
     }
 }
 
